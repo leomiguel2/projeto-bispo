bd880f23032af10484d137f5e23cf3659e7f8001
leomiguel2 <leonardofamosotimao@hotmail.com>
2025-05-26 23:32:48 +0000

    projeto bispo

---
+++ projeto bispo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 0;
+// nivel novato movimento do bispo em diagonal
+    while (i <= 10);
+
+        if(i % 2 != 0)
+        {
+            printf("diaginal a direita \n", i);
+        
+        } else {
+            
+            printf("diaginal a esquerda \n", i);
+        }
+          
+        return 0;
